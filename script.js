4c4af46353831e5f6c5adce9e7052d856559b9d9
Asabhiroop20 <abhiroopas2423@gmail.com>
2024-12-22 20:55:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+function ClockTime(){
+    const  hour=document.getElementById("hour");
+    const  minute=document.getElementById("minute");
+    const  second=document.getElementById("second");
+    const  session=document.getElementById("session");
+    const  date=document.getElementById("date");
+    const  month=document.getElementById("month");
+    const  year =document.getElementById("year");
+
+    const d = new Date();
+    var hrs = d.getHours();
+    var mins = d.getMinutes();
+    var sec = d.getSeconds();
+    if (hrs>=12){
+
+        session.textContent="PM";
+    }
+    else{
+        session.textContent="AM";
+    }
+    if (hrs>12){
+        hrs=hrs-12;
+    }
+    else{
+        hrs=hrs;
+    }
+    mins = mins < 10 ? '0' + mins : mins;
+    sec = sec < 10 ? '0' + sec : sec;
+    
+    hour.textContent=hrs;
+    minute.textContent=mins;
+    second.textContent=sec;
+    
+    
+
+
+
+}
+setInterval(ClockTime,1000);
+ClockTime();
